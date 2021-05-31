ea679da5b1f585929d98913bfe4e1017713870aa
Maria Grazielly <grazelly13@hotmail.com>
2021-05-31 16:21:24 -0300

    cadastroBD

---
+++ crud/create.php
@@ -0,0 +1,22 @@
+<?php
+//Pagina de cadastro de usuário 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "atividadebd";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO users (Aluno, Disciplina, Nota_1, Nota_2)
+  VALUES ('Carla Maria', 'IHC', '9', '8')";
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Aluno cadastrado com sucesso!";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
--- db/connect.php
+++ db/connect.php
@@ -1,13 +1,14 @@
 <?php
+//Pagina de conexão com BD
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "atividadebd";
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname"; "cherset=utf8", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname; cherset=utf8", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Conentado com, sucesso!";
+  echo "Conentado com sucesso!";
 } catch(PDOException $e) {
   echo "Falha na conexão" . $e->getMessage();
 }
